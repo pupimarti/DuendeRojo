96ddce2e75fe8cb006eb48fbe55e91a58afc01e6
Pupi Marti <42395985+pupimarti@users.noreply.github.com>
2020-03-23 22:29:42 -0300

    Update index.js

--- src/components/Home/Footer/index.js
+++ src/components/Home/Footer/index.js
@@ -6,7 +6,7 @@ function Footer(){
     return(
         <footer className="footer">
             <div className="col-900">
-                <div className="copyright">&copy; 2020 El Duende Rojo - <a href="http://www.pupimarti.eshost.com.ar" rel="noopener noreferrer">Pupi Marti</a> </div>
+                <div className="copyright">&copy; 2020 El Duende Rojo - <a href="http://www.pupimarti.eshost.com.ar" rel="noopener noreferrer" target="_blank">Pupi Marti</a> </div>
                 <ul className="menu">
                     <li className="item"><a href="https://www.facebook.com/Colonia-el-Duende-Rojo-159695604140071/"  rel="noopener noreferrer" target="_blank">Facebook</a></li>
                     <li className="item"><a href="https://www.instagram.com/coloniaelduenderojo/" rel="noopener noreferrer" target="_blank">Instagram</a></li>
